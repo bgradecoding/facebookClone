1a8e66fdaf228edc31b3392875804da32029a150
bgradecoding <iinee12@kakao.com>
2021-06-09 04:33:11 +0900

    App.js login modules add

--- src/App.js
+++ src/App.js
@@ -9,16 +9,43 @@ import Login from './pages/Login/Login'
 import SignUp from './pages/SignUp/SignUp'
 
 export default class App extends Component {
+
+  state = {
+    user: {
+      seq: 0,
+      name: "harry",
+      profileImageUrl:
+        "<https://s3.ap-northeast-2.amazonaws.com/grepp-cloudfront/programmers_imgs/learn/course9872/instructor_harry.png>"
+    },
+    posts: []
+  };
+
+  login = ( paramUser ) =>{
+    this.setState({
+      user : paramUser
+    })
+  }
+
+  loginOut = () =>{
+    this.setState({
+      user : {}
+    })
+  }
+
+  postAdd = () => {
+    this.setState({
+      ...this.state
+    })
+  }
+
   render() {
     return(
     <Router>
       <Switch>
-      <Route exact path="/" component={Home}/>
-      </Switch>
-      <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
       <Route path="/login" component={Login}/>
-      </Switch>
-      <Switch>
       <Route path="/signup" component={SignUp}/>
       </Switch>
   </Router>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import Header from './Header';
 import WritePost from './WritePost'
 import PostList from './PostList'
+import PropTypes from 'prop-types'
 
 export default class Home extends Component {
+
+  static propTypes = {
+    
+  }
+
   render() {
     return (
       <>
